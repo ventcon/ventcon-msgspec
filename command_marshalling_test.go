979af4b076573fe7948e0f42fc58c49d091c6800
Tim Neumann <git@neumann-tim.de>
2025-04-23 20:19:45 +0200

    feat: Rework both heartbeat and commands
    To better fit the implementation.
    
    Also make command messages actually marshallable.

---
+++ command_marshalling_test.go
@@ -0,0 +1,63 @@
+package msgspec
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/shoenig/test"
+	"github.com/shoenig/test/must"
+)
+
+func TestCommandMessageSerialization(t *testing.T) {
+	message := NewCommandMessage(
+		123,
+		&SetRemoteCommanderCommand{
+			RemoteCommander: true,
+		},
+	)
+	bytes, err := json.Marshal(message)
+	must.NoError(t, err)
+	jsonString := string(bytes)
+	test.Eq(t, "{\"address\":123,\"commandType\":\"setRemoteCommander\",\"command\":{\"remoteCommander\":true}}", jsonString)
+}
+
+func TestCommandMessageDeserialization(t *testing.T) {
+	message := EmptyCommandMessage()
+	err := json.Unmarshal([]byte("{\"address\":123,\"commandType\":\"setRemoteCommander\",\"command\":{\"remoteCommander\":true}}"), &message)
+	must.NoError(t, err)
+	test.Eq(t, 123, message.Address())
+
+	val, ok := message.Command().(*SetRemoteCommanderCommand)
+	test.True(t, ok)
+	test.True(t, val.RemoteCommander)
+}
+
+func TestAllCommandMessagesSerialization(t *testing.T) {
+	testCases := []struct {
+		name           string
+		commandMessage CommandMessage
+	}{
+		{"PollVentilatorNowCommand", NewCommandMessage(11, &PollVentilatorNowCommand{})},
+		{"SetRemoteCommanderCommand: true", NewCommandMessage(12, &SetRemoteCommanderCommand{RemoteCommander: true})},
+		{"SetRemoteCommanderCommand: false", NewCommandMessage(13, &SetRemoteCommanderCommand{RemoteCommander: false})},
+		{"SetVentilationModeCommand: true", NewCommandMessage(23, &SetVentilationModeCommand{VentilationMode: true})},
+		{"SetVentilationModeCommand: false", NewCommandMessage(24, &SetVentilationModeCommand{VentilationMode: false})},
+		{"SetIntakeAirLevelCommand", NewCommandMessage(34, &SetIntakeAirLevelCommand{IntakeAirLevel: 5})},
+		{"SetExhaustAirLevelCommand", NewCommandMessage(45, &SetExhaustAirLevelCommand{ExhaustAirLevel: 5})},
+		{"SetBothAirLevelCommand", NewCommandMessage(56, &SetBothAirLevelCommand{IntakeAirLevel: 7, ExhaustAirLevel: 8})},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			bytes, err := json.Marshal(tc.commandMessage)
+			must.NoError(t, err)
+
+			decodedMessage := EmptyCommandMessage()
+			err = json.Unmarshal(bytes, &decodedMessage)
+			must.NoError(t, err)
+
+			test.Eq(t, tc.commandMessage.Address(), decodedMessage.Address())
+			test.Eq(t, tc.commandMessage.Command().getCommandType(), decodedMessage.Command().getCommandType())
+			test.Eq(t, tc.commandMessage.Command(), decodedMessage.Command())
+		})
+	}
+}
