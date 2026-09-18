4350fd15a6c3a5a88dde9df90f2bb718b858ccc8
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2026-09-18 11:54:46 +0200

    feat: Rework both heartbeat and commands (#9)
    To better fit the implementation.
    
    Also make command messages actually marshallable.

--- commands.go
+++ commands.go
@@ -2,27 +2,77 @@ package msgspec
 
 const CommandTopicPrefix string = "ventcon.command"
 
-type Command string
+type CommandType string
 
 const (
-	SetIntakeAirLevel  Command = "setIntakeAirLevel"
-	SetExhaustAirLevel Command = "setExhaustAirLevel"
+	PollVentilatorNow  CommandType = "pollVentilatorNow"
+	SetRemoteCommander CommandType = "setRemoteCommander"
+	SetVentilationMode CommandType = "setVentilationMode"
+	SetIntakeAirLevel  CommandType = "setIntakeAirLevel"
+	SetExhaustAirLevel CommandType = "setExhaustAirLevel"
+	SetBothAirLevel    CommandType = "setBothAirLevel"
 )
 
-type CommandMessage struct {
-	// Address is the numeric address of the ventilator this command is for
-	Address int     `json:"address"`
-	Command Command `json:"command"`
+type Command interface {
+	getCommandType() CommandType
 }
 
-type SetIntakeAirLevelCommandMessage struct {
-	CommandMessage
+type CommandMessage interface {
+	// Address is the numeric address of the ventilator this command is for
+	Address() int
+	// Command is the actual command to be executed
+	Command() Command
+}
+
+type PollVentilatorNowCommand struct{}
+
+func (PollVentilatorNowCommand) getCommandType() CommandType {
+	return PollVentilatorNow
+}
+
+type SetRemoteCommanderCommand struct {
+	// RemoteCommander is a flag indicating whether the ventilator should be controlled by a remote commander
+	RemoteCommander bool `json:"remoteCommander"`
+}
+
+func (SetRemoteCommanderCommand) getCommandType() CommandType {
+	return SetRemoteCommander
+}
+
+type SetVentilationModeCommand struct {
+	// VentilationMode is a flag indicating whether the ventilator should be in ventilation mode
+	VentilationMode bool `json:"ventilationMode"`
+}
+
+func (SetVentilationModeCommand) getCommandType() CommandType {
+	return SetVentilationMode
+}
+
+type SetIntakeAirLevelCommand struct {
 	// IntakeAirLevel is a number from 0 to 10 indicating on which level the air intake fan should be running
 	IntakeAirLevel int `json:"intakeAirLevel"`
 }
 
-type SetExhaustAirLevelCommandMessage struct {
-	CommandMessage
+func (SetIntakeAirLevelCommand) getCommandType() CommandType {
+	return SetIntakeAirLevel
+}
+
+type SetExhaustAirLevelCommand struct {
 	// ExhaustAirLevel is a number from 0 to 10 indicating on which level the air exhaust fan should be running
 	ExhaustAirLevel int `json:"exhaustAirLevel"`
+}
+
+func (SetExhaustAirLevelCommand) getCommandType() CommandType {
+	return SetExhaustAirLevel
+}
+
+type SetBothAirLevelCommand struct {
+	// IntakeAirLevel is a number from 0 to 10 indicating on which level the air intake fan should be running
+	IntakeAirLevel int `json:"intakeAirLevel"`
+	// ExhaustAirLevel is a number from 0 to 10 indicating on which level the air exhaust fan should be running
+	ExhaustAirLevel int `json:"exhaustAirLevel"`
+}
+
+func (SetBothAirLevelCommand) getCommandType() CommandType {
+	return SetBothAirLevel
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/ventcon/ventcon-msgspec
 
 go 1.19
+
+require github.com/shoenig/test v1.12.1
+
+require github.com/google/go-cmp v0.6.0 // indirect
